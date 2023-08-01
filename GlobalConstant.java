1cd366d41f07d0c8e0c3f2abefdc1ee6555d527d
Amit Kumar <121847237+amit7974@users.noreply.github.com>
2023-08-01 12:41:10 +0530

    Add files via upload

---
+++ GlobalConstant.java
@@ -0,0 +1,48 @@
+package com.utils;
+
+public class GlobalConstant {
+public static final String USER="user";
+
+public static final String REGISTRATION="registration";
+
+public static final String URL_ACADEMICS ="/academics";
+public static final String CANDIDATE="candidate";
+public static final String COUNTRIES="countries";
+
+
+public static final String REGISTRATION2="registration2";
+
+public static final String LOADSTATESBYCOUNTRY_ID="loadStatesByCountry/{id}";
+
+public static final String ID= "id";
+
+public static final String LOADCITIESBYSTATE_ID= "loadCitiesByState/{id}";
+
+public static final String URL_REGISTRATION3= "/registration3";
+
+public static final String CANDIDATE_PROFILE="candidate_profile";
+
+public static final String USER_ACADEMICS="user_academics";
+
+public static final String MASTER_ACADEMICS="master_academics";
+
+public static final String FILEUPLOAD="fileUpload";
+
+public static final String CANDIDATE_CV="candidate_cv";
+
+public static final String SKILL="skill";
+
+public static final String USER_TECHNOLOGY= "user_technology";
+
+public static final String URL_ACTION="/action";
+
+public static final String MASTER_SKILL= "master_skill";
+
+public static final String USER_SKILL="user_skill";
+
+public static final String ACTION="action";
+
+public static final String DEMO="demo";
+
+public static final String BLANK="";
+}
