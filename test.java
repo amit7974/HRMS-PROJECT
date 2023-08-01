374828508a50df4c0041986a081a21d9b00b7c69
Amit Kumar <121847237+amit7974@users.noreply.github.com>
2023-08-01 12:40:29 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,23 @@
+package com.test;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		char[] s= {'x','j'};
+		String jh=new String(s);
+		System.out.println(jh);
+		
+	}
+
+
+	
+
+}
